fd918baa4eacde8ddf72a4894335b0d117e21c1b
Jakub Czajka <CzajkaPL@o2.pl>
2021-07-10 16:02:37 +0200

    Changed interfaces methods and improved moving system

--- src/com/company/GameOverPanel.java
+++ src/com/company/GameOverPanel.java
@@ -5,18 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
-public class GameOverPanel extends JPanel {
-
-    final private static ImageIcon snakeBackgroundIcon = new ImageIcon("images/snakeBackgroundImage.png");
-    final private static PreImage snakeImage = new PreImage(snakeBackgroundIcon, 139, 260, 622, 400);
-
-    final private static ImageIcon gameOver = new ImageIcon("images/gameOver.png");
-    final private static PreImage gameOverImage = new PreImage(gameOver, 222, 50, 457, 90);
-
-    final private static ImageIcon applesEatenI = new ImageIcon("images/applesEaten.png");
-    final private static PreImage applesEatenImage = new PreImage(applesEatenI, 50, 730, 538, 101);
-
-
+public class GameOverPanel extends JPanel implements ImageFiles{
     GameOverPanel(Integer applesEaten, String gameSize, String level) throws IOException {
         JButton score = new Button(applesEaten.toString(), 620, 730);
         score.setBounds(600, 730, 200, 101);
--- src/com/company/ImageFiles.java
+++ src/com/company/ImageFiles.java
@@ -9,4 +9,9 @@ public interface ImageFiles {
     ImageIcon difWord = new ImageIcon("images/difWord.png");
     ImageIcon sizeWord = new ImageIcon("images/sizeWord.png");
     ImageIcon snakeIcon = new ImageIcon("images/snake.png");
+    PreImage snakeImage = new PreImage(snakeBackgroundIcon, 139, 260, 622, 400);
+    ImageIcon gameOver = new ImageIcon("images/gameOver.png");
+    PreImage gameOverImage = new PreImage(gameOver, 222, 50, 457, 90);
+    ImageIcon applesEatenI = new ImageIcon("images/applesEaten.png");
+    PreImage applesEatenImage = new PreImage(applesEatenI, 50, 730, 538, 101);
 }
