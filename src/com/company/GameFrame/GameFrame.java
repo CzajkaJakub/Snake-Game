3e84ca79a2a0a6ffc08a2b4bbfae5c448e1ec952
Jakub Czajka <CzajkaPL@o2.pl>
2022-03-12 10:22:41 +0100

    Final game version

--- src/com/company/GameFrame/GameFrame.java
+++ src/com/company/GameFrame/GameFrame.java
@@ -6,13 +6,14 @@ import javax.swing.*;
 import java.io.IOException;
 
 public class GameFrame extends JFrame implements ImageFiles {
+
     public GameFrame(String level, String units) throws IOException {
         GamePanel panel = new GamePanel(level, units);
         frameSettings(panel);
     }
 
     private void frameSettings(GamePanel panel){
-        this.setIconImage(snakeIcon.getImage());
+        this.setIconImage(windowIcon.getImage());
         this.add(panel);
         this.setTitle("Snake Game!");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
